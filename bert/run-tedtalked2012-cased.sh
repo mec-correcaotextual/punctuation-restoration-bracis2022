0470fe4b64dc47292cc559e5b8853ebaaf25cc99
Tiago de Lima <tiago.blima02@gmail.com>
2022-05-28 22:18:08 -0300

    cased and uncased bert

---
+++ bert/run-tedtalked2012-cased.sh
@@ -0,0 +1,8 @@
+pip install simpletransformers
+pip install spacy==3.2.4
+pip install pandas --upgrade
+pip install wandb
+pip install zipfile36
+pip install requests
+
+python bertpunctuator.py --dataset tedtalk2012
