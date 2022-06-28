1397c52d121297d06748c8308c4df2cbfea816f9
Tiago de Lima <tiago.blima02@gmail.com>
2022-06-28 11:18:36 -0300

    update scripts

--- bertpunctuator/run-tedtalked2012.sh
+++ bertpunctuator/run-tedtalked2012.sh
@@ -2,6 +2,6 @@ pip install -r ../requirements.txt
 curl -L https://www.dropbox.com/s/gnxahozkvycfyz7/punct-tedtalk2012.zip?dl=1 > tedtalk2012.zip
 unzip tedtalk2012.zip -d ./data/
 
-python bertpunctuator.py --dataset tedtalk2012/
+python train.py --dataset tedtalk2012/
 python evaluate.py --bert_model ./outputs/
 
