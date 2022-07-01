651b9290dbeaff673e8ebcac1ccd5cf2ddc78ac5
Tiago de Lima <tiago.blima02@gmail.com>
2022-07-01 10:00:36 -0300

    update requirements.txt

--- bertpunctuator/run-tedtalked2012.sh
+++ bertpunctuator/run-tedtalked2012.sh
@@ -1,7 +1,6 @@
-pip install -r ../requirements.txt
-curl -L https://www.dropbox.com/s/gnxahozkvycfyz7/punct-tedtalk2012.zip?dl=1 > tedtalk2012.zip
-unzip tedtalk2012.zip -d ./data/
+pip install -r requirements.txt
 
+python preprocess.py
 python train.py --dataset tedtalk2012/
 python evaluate.py --bert_model ./outputs/
 
