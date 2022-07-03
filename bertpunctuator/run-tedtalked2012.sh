c60a5cd372967a82cd0aeda17ed1953564d21ef6
Tiago de Lima <tiago.blima02@gmail.com>
2022-07-03 08:58:08 -0300

    same director import

--- bertpunctuator/run-tedtalked2012.sh
+++ bertpunctuator/run-tedtalked2012.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 
 python preprocess.py
-python train.py --dataset tedtalk2012/
-python evaluate.py --bert_model ./outputs/
+python train.py --dataset tedtalk2012/ --split_data
+
 
