46c899495f5b65eca597d05ec509aa3e3c02240f
Tiago de Lima <tiago.blima02@gmail.com>
2022-07-04 21:41:09 -0300

    FIXED: bug when running: labels mismatch

--- bertpunctuator/run_bert.py
+++ bertpunctuator/run_bert.py
@@ -78,7 +78,7 @@ if args.k_fold_eval:
                 'save_eval_checkpoints': False,
                 'save_model_every_epoch': False,
                 'save_steps': -1,
-                'labels_list': ["O", "COMMA", "PERIOD", "QUESTION"],
+                'labels_list': ["O", "I-COMMA", "I-PERIOD", "I-QUESTION"],
                 'use_early_stopping': True,
                 'wandb_project': project,
                 'wandb_kwargs': {'name': 'bert-base-' + folder},
