d4fb213636f5f9fcaf24356537bb038cee9c47a8
Tiago de Lima <tiago.blima02@gmail.com>
2022-07-05 09:52:50 -0300

    allowing k-fold-eval

--- crf/run_crf.py
+++ crf/run_crf.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+
 import numpy as np
 
 from sklearn_crfsuite import CRF
@@ -15,71 +17,154 @@ BASE_DIR = '../texts/tedtalk2012/'
 
 
 def run(args):
-    # corpus_name = 'obras'
     corpus_name = 'tedtalk2012'
+    if args.k_fold_eval:
 
-    report_dir = f'./results/{corpus_name}'
-    os.makedirs(report_dir, exist_ok=True)
+        print('\nRunning k-fold evaluation...')
+        results_ents, results_micro_avg = [], []
+        for folder in os.listdir(BASE_DIR):
 
-    preprocess(BASE_DIR, args.path_to_data)
-    train_file = os.path.join(args.path_to_data, 'train.csv')
-    test_file = os.path.join(args.path_to_data, 'test.csv')
+            if os.path.isdir(os.path.join(BASE_DIR, folder)):
+                print(f'\nRunning on {folder}')
+                dataset_path = os.path.join(BASE_DIR, folder)
+                out_path = os.path.join(args.path_to_data, folder)
+                print(f'\nPreprocessing {dataset_path}')
 
-    report_file = os.path.join(report_dir, corpus_name + '_crf.csv')
+                report_dir = f'./results/{corpus_name}/{folder}'
+                os.makedirs(report_dir, exist_ok=True)
+                preprocess(BASE_DIR, args.path_to_data)
+                train_file = os.path.join(dataset_path, 'train.csv')
+                test_file = os.path.join(dataset_path, 'test.csv')
 
-    test_data = read_corpus_file(test_file, split_char=',')
-    train_data = read_corpus_file(train_file, split_char=',')
+                report_file = os.path.join(report_dir, corpus_name + '_crf.csv')
 
-    test_data_original = np.array(test_data, dtype=object)
+                test_data = read_corpus_file(test_file, split_char=',')
+                train_data = read_corpus_file(train_file, split_char=',')
 
-    print('\n  Train data:', len(train_data))
-    print('  Test data:', len(test_data))
+                test_data_original = np.array(test_data, dtype=object)
 
-    print('\nPreprocessing ...')
+                print('\n  Train data:', len(train_data))
+                print('  Test data:', len(test_data))
 
-    print('\n  Train data')
+                print('\nPreprocessing ...')
 
-    train_data = data_preprocessing(train_data)
+                print('\n  Train data')
 
-    print('  Test data')
+                train_data = data_preprocessing(train_data)
 
-    test_data = data_preprocessing(test_data)
+                print('  Test data')
 
-    X_train, y_train = convert_data(train_data)
-    X_test, y_test = convert_data(test_data)
-    pd.DataFrame.from_dict(X_train[0]).T.to_csv(f'{corpus_name}_X_train.csv', index=False)
-    print('\nExample features:', X_train[0])
-    print('Tags:', y_train[0])
+                test_data = data_preprocessing(test_data)
 
-    crf = CRF(algorithm='lbfgs', c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
+                X_train, y_train = convert_data(train_data)
+                X_test, y_test = convert_data(test_data)
+                pd.DataFrame.from_dict(X_train[0]).T.to_csv(f'{corpus_name}_X_train.csv', index=False)
+                print('\nExample features:', X_train[0])
+                print('Tags:', y_train[0])
 
-    print('\nEvaluating CRF')
+                crf = CRF(algorithm='lbfgs', c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
 
-    crf.fit(X_train, y_train)
+                print('\nEvaluating CRF')
 
-    y_pred = crf.predict(X_test)
+                crf.fit(X_train, y_train)
 
-    dict_report = classification_report(y_test, y_pred, output_dict=True)
+                y_pred = crf.predict(X_test)
 
-    data_conll = ''
+                dict_report = classification_report(y_test, y_pred, output_dict=True)
 
-    for data, real_tags, pred_tags in zip(test_data, y_test, y_pred):
-        words = data[0]
-        sent = '\n'.join('{0} {1} {2}'.format(word, real_tag, pred_tag)
-                         for word, real_tag, pred_tag in zip(words, real_tags, pred_tags))
-        sent += '\n\n'
-        data_conll += sent
+                micro_avg = dict_report.pop('micro avg')
+                micro_avg.update({'dataset_name': folder, 'classifier_name': 'bi-lstm'})
+                results_micro_avg.append(micro_avg)
 
-    print('\nReport:', dict_report)
+                dict_report.pop('macro avg')
+                dict_report.pop('weighted avg')
 
-    print('\nSaving the report in:', report_file)
+                results_ents.append(dict_report)
 
-    dump_report(dict_report, report_file)
+                data_conll = ''
 
-    script_result_file = os.path.join(report_dir, corpus_name + '_crf.tsv')
+                for data, real_tags, pred_tags in zip(test_data, y_test, y_pred):
+                    words = data[0]
+                    sent = '\n'.join('{0} {1} {2}'.format(word, real_tag, pred_tag)
+                                     for word, real_tag, pred_tag in zip(words, real_tags, pred_tags))
+                    sent += '\n\n'
+                    data_conll += sent
 
-    with open(script_result_file, 'w', encoding='utf-8') as file:
-        file.write(data_conll)
+                print('\nReport:', dict_report)
+
+                print('\nSaving the report in:', report_file)
+
+                dump_report(dict_report, report_file)
+
+                script_result_file = os.path.join(report_dir, corpus_name + '_crf.tsv')
+
+                with open(script_result_file, 'w', encoding='utf-8') as file:
+                    file.write(data_conll)
+
+    else:
+
+        report_dir = f'./results/{corpus_name}'
+        os.makedirs(report_dir, exist_ok=True)
+
+        preprocess(BASE_DIR, args.path_to_data)
+        train_file = os.path.join(args.path_to_data, 'train.csv')
+        test_file = os.path.join(args.path_to_data, 'test.csv')
+
+        report_file = os.path.join(report_dir, corpus_name + '_crf.csv')
+
+        test_data = read_corpus_file(test_file, split_char=',')
+        train_data = read_corpus_file(train_file, split_char=',')
+
+        test_data_original = np.array(test_data, dtype=object)
+
+        print('\n  Train data:', len(train_data))
+        print('  Test data:', len(test_data))
+
+        print('\nPreprocessing ...')
+
+        print('\n  Train data')
+
+        train_data = data_preprocessing(train_data)
+
+        print('  Test data')
+
+        test_data = data_preprocessing(test_data)
+
+        X_train, y_train = convert_data(train_data)
+        X_test, y_test = convert_data(test_data)
+        pd.DataFrame.from_dict(X_train[0]).T.to_csv(f'{corpus_name}_X_train.csv', index=False)
+        print('\nExample features:', X_train[0])
+        print('Tags:', y_train[0])
+
+        crf = CRF(algorithm='lbfgs', c1=0.1, c2=0.1, max_iterations=100, all_possible_transitions=True)
+
+        print('\nEvaluating CRF')
+
+        crf.fit(X_train, y_train)
+
+        y_pred = crf.predict(X_test)
+
+        dict_report = classification_report(y_test, y_pred, output_dict=True)
+
+        data_conll = ''
+
+        for data, real_tags, pred_tags in zip(test_data, y_test, y_pred):
+            words = data[0]
+            sent = '\n'.join('{0} {1} {2}'.format(word, real_tag, pred_tag)
+                             for word, real_tag, pred_tag in zip(words, real_tags, pred_tags))
+            sent += '\n\n'
+            data_conll += sent
+
+        print('\nReport:', dict_report)
+
+        print('\nSaving the report in:', report_file)
+
+        dump_report(dict_report, report_file)
+
+        script_result_file = os.path.join(report_dir, corpus_name + '_crf.tsv')
+
+        with open(script_result_file, 'w', encoding='utf-8') as file:
+            file.write(data_conll)
 
 
 if __name__ == '__main__':
